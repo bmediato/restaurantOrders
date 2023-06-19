ac8eb8d39f138e5f03690522187f96cd9a61201c
Beatriz <beatriz.mediatto2@gmail.com>
2023-06-19 16:42:09 -0300

    feat: iniciando o projeto e adicionando parte dos testes de ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,15 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    queijo = Ingredient('queijo provolone')
+
+    assert queijo.name == 'queijo provolone'
+
+    assert hash(queijo) == hash('queijo provolone')
+
+    assert Restriction.LACTOSE in queijo.restrictions
+    assert Restriction.ANIMAL_DERIVED in queijo.restrictions
+
+    assert repr(queijo) == "Ingredient('queijo provolone')"
