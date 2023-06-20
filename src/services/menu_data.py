e4e9f254ec09ecd06be93c5b318eaf7eec4dab31
Beatriz <beatriz.mediatto2@gmail.com>
2023-06-20 15:34:19 -0300

    feat: implementa class MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,4 @@
-import csv
+from csv import DictReader
 from models.dish import Dish
 from models.ingredient import Ingredient
 
@@ -6,26 +6,18 @@ from models.ingredient import Ingredient
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        self._read_menu(source_path)
         self.dishes = set()
+        dishes_map = {}
 
-        with open(source_path, mode='r') as pratos:
-            reader = csv.reader(pratos)
-            for row in reader:
-                dish = row[0]
-                price = float(row[1])
-                ingredient = row[2]
-                recipe_amount = int(row[3])
-
-                dish = self._get_or_create_dish(dish, price)
-                ingredient = Ingredient(ingredient)
+        with open(source_path, mode="r") as doc:
+            for row in DictReader(doc):
+                dish_name = row["dish"]
+                if dish_name in dishes_map:
+                    dish = dishes_map[dish_name]
+                else:
+                    dish = Dish(dish_name, float(row["price"]))
+                    dishes_map[dish_name] = dish
+                    self.dishes.add(dish)
+                ingredient = Ingredient(row["ingredient"])
+                recipe_amount = int(row["recipe_amount"])
                 dish.add_ingredient_dependency(ingredient, recipe_amount)
-
-    def _get_or_create_dish(self, name: str, price: float) -> Dish:
-        for dish in self.dishes:
-            if dish.name == name and dish.price == price:
-                return dish
-
-        dish = Dish(name, price)
-        self.dishes.add(dish)
-        return dish
